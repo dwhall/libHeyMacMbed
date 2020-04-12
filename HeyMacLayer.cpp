f29e228eddc3870765bec66eef3e794533c5227d
Dean Hall <dwhall256@gmail.com>
2020-04-11 23:04:56 -0500

    Init repo

---
+++ HeyMacLayer.cpp
@@ -0,0 +1,14 @@
+/* Copyright 2020 Dean Hall.  See LICENSE for details. */
+
+#include "mbed.h"
+
+#include "HeyMacLayer.h"
+
+
+HeyMacLayer::HeyMacLayer()
+{
+}
+
+HeyMacLayer::~HeyMacLayer()
+{
+}
---
+++ HeyMacLayer.h
@@ -0,0 +1,15 @@
+/* Copyright 2020 Dean Hall.  See LICENSE for details. */
+
+#ifndef HEYMACLAYER_H_
+#define HEYMACLAYER_H_
+
+
+class HeyMacLayer
+{
+public:
+    HeyMacLayer();
+    ~HeyMacLayer();
+
+};
+
+#endif /* HEYMACLAYER_H_ */
